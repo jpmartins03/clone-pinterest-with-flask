d803cb7831603dfd3532a2ee20bd4d80025e6cb0
jpmartins03 <jpmartins1013@gmail.com>
2025-07-19 12:26:08 -0400

    Feat - adição de rotas dinamicas para perfil e home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template ##procura no projeto a pasta chamada templates e carrega os arquivos 
-
+from flask import Flask, render_template, url_for ##procura no projeto a pasta chamada templates e carrega os arquivos 
 app = Flask(__name__) # estou criando meu aplicativo flask com o nome do arquivo que estou usando (no caso main)
 
 
@@ -10,12 +9,17 @@ app = Flask(__name__) # estou criando meu aplicativo flask com o nome do arquivo
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/perfil")
-def ProfilePage():
-    return render_template("profilepage.html")
+idade = 21
+# a tag < > diz que é uma variavel
+@app.route("/perfil/<usuario>")
+def ProfilePage(usuario):
+    # posso passar a variavel que a função recebe de parametro para o render template
+    # desde que tenha uma variavel na página renderizada para receber a variavel parametro
+    return render_template("profilepage.html", usuario=usuario, idade=idade)
 
 
 # coloca o site no ar, se eu executar o arquivo e ele retornar o nome dele mesmo,
 # assim garanto que ele nao seja executado em imports, apenas nele mesmo
 if __name__ == "__main__":
     app.run(debug=True)
+ 
