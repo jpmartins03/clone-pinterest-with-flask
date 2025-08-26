f58e999b12f8fc1d39bcbec70a40bd04406a48ac
jpmartins03 <jpmartins1013@gmail.com>
2025-08-26 14:40:15 -0400

    FEAT - Criação dos primeiros formulários e suas rotas

--- fakePinterest/forms.py
+++ fakePinterest/forms.py
@@ -1,19 +1,19 @@
 # arquivo que define a estrutura dos formularios
-from flask wtf import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from models import User
+from fakePinterest.models import User
 
 class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
-    password = PasswordField("Senha", valdiator=[DataRequired()])
+    password = PasswordField("Senha", validators=[DataRequired()])
     confirm_button = SubmitField("Fazer Login")
 
 class FormCriarConta(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
-    username = StrinbgField("Nome de usuário", validators=[DataRequired()])
-    password = PasswordField("Senha", validators=[DataRequired(), lenght(min=6, max=16)])
-    password_confirm = PasswordField("Confirmar Senha", valdiators=[DataRequired(), EqualTo("password")])
+    username = StringField("Nome de usuário", validators=[DataRequired()])
+    password = PasswordField("Senha", validators=[DataRequired(), Length(min=6, max=16)])
+    password_confirm = PasswordField("Confirmar Senha", validators=[DataRequired(), EqualTo("password")])
     confirm_button = SubmitField("Criar Conta")
 
     ##validação de email
--- fakePinterest/routes.py
+++ fakePinterest/routes.py
@@ -5,13 +5,23 @@ from fakePinterest import app
 from flask import render_template
 from flask_login import login_required
 
+#importa os formularios do meu arquivo
+from fakePinterest.forms import FormLogin, FormCriarConta
+
 # rota da homepage
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form = formlogin)
 
 idade = 21
 # a tag < > diz que é uma variavel
+
+@app.route("/criar_conta", methods=["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form = formcriarconta)
+
 
 @app.route("/perfil/<usuario>")
 @login_required
